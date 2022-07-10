ed6d3158eb444ab21ee1bdb9181460e2f1a1960d
Axweil <axweil98@mail.ru>
2022-07-10 22:39:28 +0500

    true

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1.getMessage());
+
+        boolean beans = bean == bean1;
+        System.out.println(beans);
+
+        boolean cats = cat == cat1;
+        System.out.println(cats);
     }
 }
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
  
     private String message;
- 
     public String getMessage() {
         return message;
     }
